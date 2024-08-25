6f2740193db14bc96e27481289e53055c0f21071
Adriteyo Das <141396531+Addycted@users.noreply.github.com>
2024-08-25 18:41:40 +0530

    Update Postfix2Infix.c

--- Stacks/Postfix2Infix.c
+++ Stacks/Postfix2Infix.c
@@ -10,7 +10,7 @@ typedef struct {
 } Stack;
 
 // Initialize the stack
-void initStack(Stack *s) {
+void init(Stack *s) {
     s->top = -1;
 }
 
@@ -34,6 +34,7 @@ void pop(Stack *s, char *value) {
         strcpy(value, s->data[(s->top)--]);
     } else {
         printf("Stack underflow!\n");
+        value[0] = '\0'; // Set to empty string
     }
 }
 
@@ -42,71 +43,47 @@ int isOperator(char c) {
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
 }
 
-// Concatenate strings
-void concatStrings(char *result, char *str1, char op, char *str2) {
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    int i, j;
-
-    // Add '('
-    result[0] = '(';
-    
-    // Copy str2
-    for (i = 1; i <= len2; i++) {
-        result[i] = str2[i - 1];
-    }
-    
-    // Add operator
-    result[i++] = op;
-    
-    // Copy str1
-    for (j = 0; j < len1; j++) {
-        result[i++] = str1[j];
-    }
-    
-    // Add ')'
-    result[i] = ')';
-    result[i + 1] = '\0';
-}
-
-// Convert postfix expression to infix expression
-void postfixToInfix(char *postfix, char *infix) {
+int main(){
     Stack stack;
-    initStack(&stack);
+    init(&stack);
+    char postfix[MAX] = "321*+9-";
+    char infix[MAX];
     char temp[MAX];
     
     for (int i = 0; postfix[i] != '\0'; i++) {
         char token = postfix[i];
         
-        if (isOperator(token)) {
-            // Pop two operands from the stack
-            char op1[MAX], op2[MAX];
-            pop(&stack, op1);
-            pop(&stack, op2);
-            
-            // Create a new string in the form "(op2 operator op1)"
-            concatStrings(temp, op2, token, op1);
-            
-            // Push the new string back onto the stack
-            push(&stack, temp);
+        if (!isOperator(token)) {
+            // Push the operand (as a string)
+            char operand[2] = {token, '\0'};
+            push(&stack, operand);
         } else {
-            // Operand: push it onto the stack
-            temp[0] = token;
-            temp[1] = '\0';
+            char val1[MAX];
+            char val2[MAX];
+            
+            // Pop two operands
+            pop(&stack, val1);
+            pop(&stack, val2);
+            
+            // Form the new string with the operator
+            strcpy(temp, "(");
+            strcat(temp, val2);
+            char operator[2] = {token, '\0'};
+            strcat(temp, operator);
+            strcat(temp, val1);
+            strcat(temp, ")");
+            
+            // Push the result back to the stack
             push(&stack, temp);
         }
     }
     
-    // The final infix expression will be the top of the stack
+    // Pop the final infix expression
     pop(&stack, infix);
-}
-
-int main() {
-    char postfix[MAX] = "321*+9-";
-    char infix[MAX];
     
-    postfixToInfix(postfix, infix);
-    printf("Infix expression: %s\n", infix);
+    // Print the infix expression
+    puts(infix);
     
     return 0;
 }
+
