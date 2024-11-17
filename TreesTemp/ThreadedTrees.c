3c1dfc424181d6977430f036e85a78472ef92544
Adriteyo Das <141396531+Addy-Da-Baddy@users.noreply.github.com>
2024-11-17 10:55:00 +0530

    Update ThreadedTrees.c

--- TreesTemp/ThreadedTrees.c
+++ TreesTemp/ThreadedTrees.c
@@ -37,3 +37,53 @@ void tinorder(node *head) {
         temp = in_suc(temp);
     }
 }
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a binary tree node
+typedef struct node {
+    int data;
+    struct node *left, *right;
+} node;
+
+// Function to create a new node
+node* createNode(int data) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Morris Traversal for inorder traversal
+void morrisTraversal(node* root) {
+    node* current = root;
+
+    while (current != NULL) {
+        if (current->left == NULL) {
+            // No left child, print the current node and move to the right
+            printf("%d ", current->data);
+            current = current->right;
+        } else {
+            // Find the inorder predecessor of current
+            node* predecessor = current->left;
+            while (predecessor->right != NULL && predecessor->right != current) {
+                predecessor = predecessor->right;
+            }
+
+            if (predecessor->right == NULL) {
+                // Create a thread to the current node
+                predecessor->right = current;
+                current = current->left;
+            } else {
+                // Thread exists, remove it and process the current node
+                predecessor->right = NULL;
+                printf("%d ", current->data);
+                current = current->right;
+            }
+        }
+    }
+}
+
